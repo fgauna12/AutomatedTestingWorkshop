0fbab03b73f96f6715ab582b72b7a107b8c6fe8c
Facundo Gauna <facundo@NebbiaTech.com>
2019-04-24 10:45:16 -0400

    added logic to read from the database

--- src/KetoPal.Api/Controllers/ProductsController.cs
+++ src/KetoPal.Api/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace KetoPal.Api.Controllers
 {
@@ -11,20 +14,26 @@ namespace KetoPal.Api.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // GET api/products
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<List<Product>> Get()
+        public async Task<ActionResult<List<Product>>> Get()
         {
-            return Ok(
-                new List<Product>()
-                {
-                    new Product()
-                    {
-                        Name = "Oreos"
-                    }
-                }
-            );
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+
+                var products = await connection.QueryAsync<Product>("Select [long_name] 'Name' from dbo.Products");
+
+                return Ok(products.ToList());
+            }
         }
     }
 
