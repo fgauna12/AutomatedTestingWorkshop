116f459edc6da16145f6bc38b71f508c6657fdf5
Facundo Gauna <facundo@NebbiaTech.com>
2019-04-24 09:51:03 -0400

    inital commit

---
+++ src/KetoPal.Api/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KetoPal.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET api/products
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Product>> Get()
+        {
+            return Ok(
+                new List<Product>()
+                {
+                    new Product()
+                    {
+                        Name = "Oreos"
+                    }
+                }
+            );
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+    }
+}
