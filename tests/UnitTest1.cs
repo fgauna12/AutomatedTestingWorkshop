2bdf1f7f39752f52bfd6c751975f9f6482df3712
Facundo Gauna <facundo@NebbiaTech.com>
2019-04-26 11:45:07 -0400

    added some more characterization tests

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -22,5 +22,84 @@ namespace KetoPal.Tests
 
             ActionResult<List<Product>> response = await productsController.Get(0);
         }
+
+        [TestMethod]
+        public async Task x1()
+        {
+            //do they all have carb content?
+
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(x => x.GetSection("ConnectionStrings")["FoodDb"])
+                .Returns("Server=10.0.75.1;Database=Foods;User Id=SA;Password=#newPass1");
+
+            var productsController = new ProductsController(configuration.Object);
+
+            ActionResult<List<Product>> response = await productsController.Get(0);
+
+            var result = response.Result as ObjectResult;
+            var products = result?.Value as List<Product>;
+            Assert.IsNotNull(products);
+            Assert.IsTrue(products.TrueForAll(x => x.Carbs > 0.0), "some products have carbs");
+        }
+
+        [TestMethod]
+        public async Task x2()
+        {
+            //do they all have manufacturer?
+
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(x => x.GetSection("ConnectionStrings")["FoodDb"])
+                .Returns("Server=10.0.75.1;Database=Foods;User Id=SA;Password=#newPass1");
+
+            var productsController = new ProductsController(configuration.Object);
+
+            ActionResult<List<Product>> response = await productsController.Get(0);
+
+            var result = response.Result as ObjectResult;
+            var products = result?.Value as List<Product>;
+            Assert.IsNotNull(products);
+            Assert.IsTrue(products.TrueForAll(x => x.Manufacturer != null), "Some products don't have manufacturers");
+        }
+
+        [TestMethod]
+        public async Task x3()
+        {
+            //what happens if I provide a user id?
+
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(x => x.GetSection("ConnectionStrings")["FoodDb"])
+                .Returns("Server=10.0.75.1;Database=Foods;User Id=SA;Password=#newPass1");
+
+            var productsController = new ProductsController(configuration.Object);
+
+            ActionResult<List<Product>> response = await productsController.Get(1);
+
+            var result = response.Result as ObjectResult;
+            var products = result?.Value as List<Product>;
+            Assert.IsNotNull(products);
+            
+        }
+
+        [TestMethod]
+        public async Task x4()
+        {
+            //there seems to be some filtering for some users
+
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(x => x.GetSection("ConnectionStrings")["FoodDb"])
+                .Returns("Server=10.0.75.1;Database=Foods;User Id=SA;Password=#newPass1");
+
+            var productsController = new ProductsController(configuration.Object);
+
+            ActionResult<List<Product>> response0 = await productsController.Get(0);
+            ActionResult<List<Product>> response1 = await productsController.Get(1);
+
+            var result0 = response0.Result as ObjectResult;
+            var products0 = result0?.Value as List<Product>;
+            var result1 = response1.Result as ObjectResult;
+            var products1 = result1?.Value as List<Product>;
+            Assert.IsTrue(products0.Count > products1.Count);
+
+        }
     }
 }
