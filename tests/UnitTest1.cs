3674b6a092da89c7f6b5165272ea93c6027bb22d
Facundo Gauna <facundo@NebbiaTech.com>
2019-04-26 11:21:43 -0400

    working characterization test

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,4 +1,10 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using KetoPal.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace KetoPal.Tests
 {
@@ -6,9 +12,15 @@ namespace KetoPal.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void x()
+        public async Task x()
         {
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(x => x.GetSection("ConnectionStrings")["FoodDb"])
+                .Returns("Server=10.0.75.1;Database=Foods;User Id=SA;Password=#newPass1");
 
+            var productsController = new ProductsController(configuration.Object);
+
+            ActionResult<List<Product>> response = await productsController.Get(0);
         }
     }
 }
